2e4ffc606c7251610059696e22275bb040019041
Alex Todd <alex@atodd.io>
2016-02-28 01:55:56 -0800

    Add web login for hosting a party

--- DjCollab.Api/DjCollab.Api/Controllers/HomeController.cs
+++ DjCollab.Api/DjCollab.Api/Controllers/HomeController.cs
@@ -6,8 +6,15 @@ using System.Web.Mvc;
 
 namespace DjCollab.Api.Controllers
 {
+    /// <summary>
+    /// View Controller for Home
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Returns view for the index
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             ViewBag.Title = "DJCollab";
---
+++ DjCollab.Api/DjCollab.Api/Controllers/HostPartyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DjCollab.Api.Controllers
+{
+    /// <summary>
+    /// View Controller for Hosting a Party
+    /// </summary>
+    public class HostPartyController : Controller
+    {
+        /// <summary>
+        /// Returns the callback after login
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Callback()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Returns the main host a party view
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Host a Party";
+            return View();
+        }
+    }
+}
