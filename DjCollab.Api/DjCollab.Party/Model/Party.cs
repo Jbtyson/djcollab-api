50fcbd113ca8c7a1297c687079eb1e1502bfe120
James Tyson <jbtyson@ksu.edu>
2016-02-28 00:13:25 -0600

    removed extraneous party model stuff

--- DjCollab.Api/DjCollab.Party/Model/Party.cs
+++ DjCollab.Api/DjCollab.Party/Model/Party.cs
@@ -9,9 +9,6 @@ namespace DjCollab.Party.Model
         public int Id { get; set; }
         public int HostId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
         public IList<int> SongList { get; set; }
     }
 }
