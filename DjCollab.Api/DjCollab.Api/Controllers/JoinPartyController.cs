17ccc647840813eb0779c534ac531dfa7b24a053
Alex Todd <alex@atodd.io>
2016-02-28 08:04:14 -0800

    Remove host, add join

---
+++ DjCollab.Api/DjCollab.Api/Controllers/JoinPartyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DjCollab.Api.Controllers
+{
+    public class JoinPartyController : Controller
+    {
+        // GET: JoinParty
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Join a Party";
+            return View();
+        }
+    }
+}
