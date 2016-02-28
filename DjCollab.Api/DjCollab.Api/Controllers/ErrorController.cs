bc69125f3e58e178608f2278e51b10aeb44d1c9f
James Tyson <jbtyson@ksu.edu>
2016-02-28 02:17:04 -0600

    added error endpoint

---
+++ DjCollab.Api/DjCollab.Api/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace DjCollab.Api.Controllers
+{
+    [RoutePrefix("api/v1/error")]
+    public class ErrorController : ApiController
+    {
+        public static IList<string> Errors = new List<string>();
+
+        public ErrorController()
+        {
+        }
+
+        /// <summary>
+        /// Error log.
+        /// </summary>
+        /// <returns>List of errors since last server restart.</returns>
+        [HttpGet]
+        [Route("")]
+        [ResponseType(typeof (IList<string>))]
+        public IHttpActionResult Get()
+        {
+            return Ok(Errors);
+        }
+    }
+}
